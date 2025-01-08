e9523d161e5f0c7d535565e23c61fe03dec64280
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-08 14:03:40 +0100

    fix

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  return new Response(`Hello world!`);
 }
