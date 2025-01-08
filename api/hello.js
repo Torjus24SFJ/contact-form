fb3399f07333c5a2eff83938ebd215110cf88409
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-08 14:00:31 +0100

    hellojs

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,5 @@
-export function GET() {
-    return "Hello World!"
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+ 
+export function GET(request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
