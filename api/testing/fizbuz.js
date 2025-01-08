dd3a969974a33e33590a24a5cd148d752e50cc14
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-08 14:05:48 +0100

    fixbuz

--- api/testing/fizbuz.js
+++ api/testing/fizbuz.js
@@ -1,3 +1,5 @@
-export function GET() {
-    return new Response("Foobar")
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
+
+export function GET(request) {
+  return new Response("Foobar");
 }
