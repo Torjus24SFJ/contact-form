3518005a4a28224e2ab18ff26d38ead61cfb07c0
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-08 14:43:10 +0100

    resend_client

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,17 @@
+import { Resend } from 'resend';
+
+const apiKey = process.env.RESEND_API_KEY
+const resend = new Resend(apiKey)
+
 export function GET(request) {
-  return new Response(`Hello world!`);
+    resend.emails.send({
+        from: "contact@mypage.no",
+        to: "Torjus24sfj@gmail.com",
+        subject: "From the future",
+        html: `
+        <h1>This is me from the future</h1>
+        <p>Somebody poisoned the coffee</p>
+        <p>Do not drink the coffee!</p>
+        `,
+    })
 }
