53de01f36b387bbf6301a25b39f6a0e4bc83a4f3
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-08 14:07:16 +0100

    anotherfix

--- api/testing/fizbuz.js
+++ api/testing/fizbuz.js
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
 export function GET(request) {
-  return new Response("Foobar");
+  return new Response('Foobar');
 }
