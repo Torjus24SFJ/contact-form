538c50734a4de6452567d86dae38d6b5ab86fbd8
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-09 11:35:12 +0100

    html fields

--- api/contact-mail.js
+++ api/contact-mail.js
@@ -4,22 +4,18 @@ const apiKey = process.env.RESEND_API_KEY;
 const resend = new Resend(apiKey);
 
 export async function POST(request) {
-  const data = await request.json();
-  console.log(data);
+  const contactFormData = await request.json();
 
-  // console.log("Starting sending mail via resend");
+  const result = await resend.emails.send({
+    from: "Lars <contact@resend.dev>",
+    to: "Torjus24sfj@gmail.com",
+    subject: contactFormData.email,
+    html: `
+      <h1>${contactFormData.email}</h1>
+      <p>${contactFormData.message}</p>
+      `,
+    });
+    
+    // return new Response("Hello World!");
 
-  // const result = await resend.emails.send({
-  //   from: "Lars <contact@resend.dev>",
-  //   to: "Torjus24sfj@gmail.com",
-  //   subject: "Contact wanted message",
-  //   html: `
-  //     <h1>Message from somebody</h1>
-  //     <p>They said nice things about me</p>
-  //   `,
-  // });
-
-  // console.log(result.error.message);
-
-  return new Response("Hello World!");
 }
