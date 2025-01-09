fb7dba962da733f7bdfac0181a103c3a4fe3daaf
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-09 10:05:53 +0100

    json something

--- api/contact-mail.js
+++ api/contact-mail.js
@@ -4,6 +4,7 @@ const apiKey = process.env.RESEND_API_KEY;
 const resend = new Resend(apiKey);
 
 export async function POST(request) {
+    const data = await request.json();
     console.log(request);
     
 
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const response = await fetch("/api/contact-mail")
+const contactMessage = {
+    email: "kevin@home.alone",
+    message: "Deez nuts",
+}
+const response = await fetch("/api/contact-mail" 
+,{ 
+     method: "POST",
+     body: JSON.stringify(contactMessage),
+});
 const data = await response.text()
 console.log(data)
