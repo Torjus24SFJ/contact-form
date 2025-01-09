5a0c45070c85f627a4b373ffd8dbf0fda9b4bc91
Torjus Tveten <Torjus24sfj@gmail.com>
2025-01-09 11:15:17 +0100

    eventlistener queryselector check

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const contactMessage = {
-    email: "kevin@home.alone",
-    message: "Freeze M****ers!",
-  };
-  
-  const response = await fetch("/api/contact-mail", {
-    method: "POST",
-    body: JSON.stringify(contactMessage),
-  });
-  const data = await response.text();
-  console.log(data);
+const formElement = document.querySelector("contact");
+if (formElement !== null) {
+    throw Error("Hey smartass! The id is wrong!")
+}
+
+formElement.addEventlistener("submit", async(event) => {
+    // prevent refresh
+    event.preventdefault();
+    
+    // get input values
+    const contactMessage = {
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      };
+      
+      // send message to serverless function
+      const response = await fetch("/api/contact-mail", {
+        method: "POST",
+        body: JSON.stringify(contactMessage),
+      });
+
+      //check if successful
+      if (response.ok){
+        formElement.reset();
+      }
+});
